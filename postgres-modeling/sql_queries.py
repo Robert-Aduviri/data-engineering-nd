c364a3bece3841bc1cdf02836fd8638de32fc14e
Robert <robert.alonso.ac@gmail.com>
2021-06-19 02:00:44 -0500

    Add SQL queries for creating the tables

--- postgres-modeling/sql_queries.py
+++ postgres-modeling/sql_queries.py
@@ -1,26 +1,73 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
+# The only difference between TEXT and VARCHAR(n) is that you can limit the maximum
+# length of a VARCHAR column, for example, VARCHAR(255) does not allow inserting a
+# string more than 255 characters long.
+
+# The main reason for primary and foreign keys is to enforce data consistency.
+
 songplay_table_create = """
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id VARCHAR,
+    start_time INT,
+    user_id VARCHAR,
+    level VARCHAR(20),
+    song_id VARCHAR,
+    artist_id VARCHAR,
+    session_id INT,
+    location VARCHAR,
+    user_agent VARCHAR
+)
 """
 
 user_table_create = """
+CREATE TABLE IF NOT EXISTS users (
+    user_id VARCHAR,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    gender VARCHAR,
+    level VARCHAR
+)
 """
 
 song_table_create = """
+CREATE TABLE IF NOT EXISTS songs (
+    song_id VARCHAR,
+    title VARCHAR,
+    artist_id VARCHAR,
+    year VARCHAR,
+    duration VARCHAR
+)
 """
 
 artist_table_create = """
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id VARCHAR,
+    name VARCHAR,
+    location VARCHAR,
+    latitude VARCHAR,
+    longitude VARCHAR
+)
 """
 
 time_table_create = """
+CREATE TABLE IF NOT EXISTS time (
+    start_time INT,
+    hour INT,
+    day INT,
+    week INT,
+    month INT,
+    year INT,
+    weekday INT
+)
 """
 
 # INSERT RECORDS
